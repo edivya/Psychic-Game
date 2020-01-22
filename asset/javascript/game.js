729db5f36263564e9c626c3497815903bfd1de4f
edivya <enjamuridivya@gmail.com>
2020-01-21 16:48:23 -0800

    changes made on javascript

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -20,3 +20,25 @@ function theGame() {
 	console.log(computerChoice); //test
 		
 	checkIfCorrect();
+	function checkIfCorrect() {
+
+		document.onkeyup = function(event) {
+
+			//turns user's unicode into corresponding alphanumeric key,
+			//then turns it into a lowercase string.
+			var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+
+
+			//if statement to prevent user from choosing a key that's not part of
+			//the alphabet. a = unicode 65, z = unicode 90.
+			if (event.keyCode < 65 || event.keyCode > 90) {
+				alert("Invalid Entry");
+
+			//else/if statement to stop user from guess a letter they already guessed
+			//and to stop the guesses counter from being deducted for a previous guess
+			} else if (yourGuess.indexOf(userChoice) >=0) {
+				alert("You already guessed that!");
+
+			//if the user guesses correctly
+			} else if (userChoice === computerChoice) {
+				console.log("You win."); //test
