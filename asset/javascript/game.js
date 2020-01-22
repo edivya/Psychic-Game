18009c3d36a0b0942ccaf849cb9cfc9e8600be8d
edivya <enjamuridivya@gmail.com>
2020-01-21 16:47:43 -0800

    changes made on javascript

--- asset/javascript/game.js
+++ asset/javascript/game.js
@@ -1,51 +1,22 @@
-// The specific letters that the user typed.
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// Setting for zero
+//variables for game
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var letterUser = [];
-var eachofLetters = null;
+var yourGuess = []; 
+var computerChoice;
 
 
-// Sets the computerGuess variable equal to a random choice from the computerChoice array.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-function countGuessesLeft() {
-	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-}
+//to initiate the game
+theGame();
 
-function farUserGuesses() {
-	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
-}
 
-countGuessesLeft();
+function theGame() {
+	//letter randomizer
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var randomLetter = alphabet[Math.floor(Math.random() * 26)];
+	var computerChoice = randomLetter;
 
-var restart = function() {
-	guessesLeft = 9;
-	letterUser = [];
-	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
-
-// When the user presses a key, it will run the following function..
-document.onkeyup = function(event) {
-	guessesLeft--;
-
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-	letterUser.push(userGuess);
-	countGuessesLeft();
-	farUserGuesses();
-
-	if (userGuess === computerGuess){
-		wins++;
-		document.querySelector("#wins").innerHTML = "Wins: " + wins;
-		restart();
-	} 
-	else if (guessesLeft === 0) {
-		losses++;
-		document.querySelector("#lose").innerHTML = "Loses: " + losses;
-		restart();
-	}
-};
+	console.log(computerChoice); //test
+		
+	checkIfCorrect();
